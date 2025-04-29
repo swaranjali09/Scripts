049cf90a7c2afe4cd7df70da4470cf7ec8d24bf8
swaranjali09 <151865392+swaranjali09@users.noreply.github.com>
2025-04-29 14:22:57 +0530

    Update Install awscli.sh

--- Install awscli.sh
+++ Install awscli.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+# Download AWS CLI v2
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+# Unzip the installer
 unzip awscliv2.zip
+
+# Install or update AWS CLI
 sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
+
+# Clean up
+rm -rf aws awscliv2.zip
+
+# Verify installation
+aws --version
