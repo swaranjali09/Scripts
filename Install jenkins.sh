c839bf9991ef402167bc6c285bb0b69ea1027649
swaranjali09 <151865392+swaranjali09@users.noreply.github.com>
2025-04-29 14:29:05 +0530

    Update Install jenkins.sh

--- Install jenkins.sh
+++ Install jenkins.sh
@@ -1,16 +1,17 @@
-#! /bin/bash
+#!/bin/bash
 # For Ubuntu 22.04
 
-# Intsalling Java
+# Update packages
 sudo apt update -y
+
+# Install Java (OpenJDK 11)
 sudo apt install openjdk-11-jre -y
 java --version
 
-# Installing Jenkins
-curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update -y
-sudo apt-get install jenkins -y
+# Add Jenkins repository and key
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# Update package index and install Jenkins
+sudo apt update -y
+sudo apt install jenkins -y
